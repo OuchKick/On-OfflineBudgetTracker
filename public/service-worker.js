d4f8cc60cdd9d37e3ea6e7ae389e8111a1124415
Adam Reed <thirdeyeadam@gmail.com>
2021-05-14 17:21:41 -0500

    updating files - wasn't working on heroku

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,80 +1,70 @@
+
+
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
-    '/index.js',
-    '/db.js',
-    '/manifest.webmanifest',
-  ];
-  
-  const CACHE_NAME = "static-cache-v2";
-  const DATA_CACHE_NAME = "data-cache-v1";
-  
-  // install
-  self.addEventListener("install", function(evt) {
+    "/",
+    "/index.html",
+    "/index.js",
+    "/db.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+    "/styles.css",
+    "/manifest.webmanifest",
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+self.addEventListener("install", function (evt) {
+    console.log("attempting to install service worker and cache static assets");
     evt.waitUntil(
-      caches.open(CACHE_NAME).then(cache => {
-        console.log("Your files were pre-cached successfully!");
-        return cache.addAll(FILES_TO_CACHE);
-      })
+      caches.open(DATA_CACHE_NAME).then((cache) => { return cache.add("/api/transaction")})
     );
-  
+    evt.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {return cache.addAll(FILES_TO_CACHE)})
+    );
     self.skipWaiting();
   });
-  
-  // Activate service worker and clear out old cache data
-  self.addEventListener("activate", function(evt) {
-    evt.waitUntil(
-      caches.keys().then(keyList => {
-        return Promise.all(
-          keyList.map(key => {
-            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-              console.log("Removing old cache data", key);
-              return caches.delete(key);
-            }
-          })
-        );
-      })
+
+
+self.addEventListener("activate", function(evt) {
+evt.waitUntil(
+    caches.keys().then(keyList => {
+    return Promise.all(
+        keyList.map(key => {
+        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+        }
+        })
     );
-  
-    // Claim = add / add new cache
-    self.clients.claim();
-  });
-  
-  // fetch
-  self.addEventListener("fetch", function(evt) {
-    // cache successful requests to the API
-    if (evt.request.url.includes("/api/")) {
-      evt.respondWith(
+    })
+);
+
+self.clients.claim();
+});
+
+self.addEventListener('fetch', evt => {
+if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
         caches.open(DATA_CACHE_NAME).then(cache => {
-          return fetch(evt.request)
+            return fetch(evt.request)
             .then(response => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
-              }
-  
-              return response;
+                if (response.status === 200) {
+                    cache.put(evt.request.url, response.clone());
+                }
+                return response;
             })
             .catch(err => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
+                return cache.match(evt.request);
             });
         }).catch(err => console.log(err))
-      );
-  
-      return;
-    }
-  
-    // if the request is not for the API, serve static assets using "offline-first" approach.
-    // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
-    evt.respondWith(
-      caches.match(evt.request).then(function(response) {
-        return response || fetch(evt.request);
-      })
     );
-  });
-  
-  
+
+    return;
+}
+evt.respondWith(
+    caches.match(evt.request).then(function(response) {
+        return response || fetch(evt.request);
+    })
+);
+});
